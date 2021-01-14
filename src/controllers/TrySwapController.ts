5aeda4cc94ba424b2966e1ffc6b4352bacc3cfc4
DevDosSantosLucas <lucas1995ls@outlook.com>
2021-01-14 09:13:21 -0300

    commit

--- src/controllers/TrySwapController.ts
+++ src/controllers/TrySwapController.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import { string } from 'yup/lib/locale';
 
 import Swap from '../models/Swap';
+import SwapView from '../views/SwapView'
 
-// interface ItemId {
-//     item_id: string;
-//   }
+interface ItemId {
+    item_id: string;
+    targed_item_id: string;
+  }
   
 class TrySwapController {
 
     async trySwap(request: Request, response: Response){
+     
             
-            const { targed_item_id } = request.params;
-            const { item_id } = request.body.item_id;
+            const { targed_item_id } = request.params ;//as ItemId ;
+            const { item_id } = request.body.item_id ; //as ItemId ;
+              
 
             // console.log(targed_item_id)
             // console.log(item_id)
@@ -27,8 +32,15 @@ class TrySwapController {
         item_id,
     }
 
+    const item = await swapRepository.findOneOrFail({ where: { item_id} }  );
+    if(item) return response.status(201).json("Item ja inserido ");
 
-    const newSwap = swapRepository.create(swap);
+    const newSwap = swapRepository.create({
+      targed_item_id,
+      item_id,
+    });
+    
+    // delete item_id.user_id.password
   
     await swapRepository.save(swap);
 
@@ -48,20 +60,23 @@ class TrySwapController {
             const { item_id } = request.params;
             const swapRepository =getRepository(Swap);
     
-            // const item = await swapRepository.find( {
-            //     relations: ["item_id","targed_item_id"] 
-            //   } );
+      
               const item = await swapRepository.findOneOrFail({ 
-                relations: [ "item_id", "targed_item_id" ] ,// falta mostrar todas a s infos dos Items
-                where: { item_id},
+                relations: [   "item_id", "targed_item_id",
+                       "item_id.images" , "targed_item_id.images",
+                       "item_id.user_id", "targed_item_id.user_id"] ,
+                where: { item_id},      
                 }
+                
                );
-              console.log(item)
-            return response.status(201).json(item);
+              
+              //  delete item.item_id.user_id.password
+            
+            return response.status(201).json(SwapView.render(item));
+            
 
     }
 }
-
 
 export default new TrySwapController()
 
--- src/views/SwapView.ts
+++ src/views/SwapView.ts
@@ -1,9 +1,11 @@
+import Item from "../models/Item";
 import Swap from "../models/Swap";
-
+import ItemView from './ItemView'
 export default {
   render(swap: Swap) {
     return{
       swap_id: swap.swap_id,
+      
       item_id: swap.item_id,
       targed_item_id:  swap.targed_item_id
     };
